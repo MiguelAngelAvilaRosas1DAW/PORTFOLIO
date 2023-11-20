7acd8a541dd25ad5e5f21b94a6b37ea34b1b0152
Miguel Ángel Ávila Rosas <148963870+MiguelAngelAvilaRosas1DAW@users.noreply.github.com>
2023-11-20 10:52:34 +0100

    Add files via upload

---
+++ HALLOWEEN/js/halloween.js
@@ -0,0 +1,27 @@
+/********************************************/
+/* Hojas de JavaScript                      */
+/* Autor: Miguel Ángel Ávila Rosas          */
+/* Version: 1.2                             */
+/* Creado el: 6 de noviembre de 2023        */
+/* Actualizado el: 15 de noviembre de 2023  */
+/********************************************/
+
+function mostrar(elemento) { 
+ el=document.getElementById (elemento);
+ el.style.visibility="visible";
+}
+
+function ocultar(elemento) { 
+ el=document.getElementById (elemento);
+ el.style.visibility="hidden";
+}
+ 
+ function sonar(elemento) {
+ var audio = document.getElementById(elemento);
+ audio.play();
+ }
+
+ function pausar(elemento) {
+ var audio = document.getElementById(elemento);
+ audio.pause();
+ }
