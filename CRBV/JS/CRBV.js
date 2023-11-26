87f7e09f14974ef551759f79c10070bbb2e1cd1f
Miguel Ángel Ávila Rosas <148963870+MiguelAngelAvilaRosas1DAW@users.noreply.github.com>
2023-11-26 11:45:59 +0100

    Add files via upload

---
+++ CRBV/JS/CRBV.js
@@ -0,0 +1,12 @@
+/********************************************/
+/* Hojas de JavaScript                      */
+/* Autor: Miguel Ángel Ávila Rosas          */
+/* Version: 0.1                             */
+/* Creado el: 23 de noviembre de 2023       */
+/* Actualizado el: 23 de noviembre de 2023  */
+/********************************************/
+
+function mostrar(elemento) { 
+  el=document.getElementById (elemento);
+  el.style.visibility="visible";
+ }
